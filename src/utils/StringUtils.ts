bd999e69b2708ff7fbda7e4a787bbba8f7f3d6b8
dacil <davidcisnelaz@gmail.com>
2025-02-20 15:34:52 -0600

    Adde other functions for string utils

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -6,4 +6,25 @@ export class StringUtils {
   public static isNotEmpty(str: string | null | undefined): boolean {
     return str !== undefined && str !== null && str.trim().length > 0;
   }
+
+  public static removeNonASCII(str: string): string {
+    return str.replace(/[^\x00-\x7F]/g, '');
+  }
+
+  public static truncateString(str: string, maxLength: number): string {
+    if (str.length > maxLength) {
+      return str.slice(0, maxLength);
+    }
+    return str;
+  }
+
+  public static validatePositiveNumber(value: string): string {
+    return this.isPositiveNumber(value) ? value : '';
+  }
+
+  // Allow numbers greater than 0, including decimals
+  public static isPositiveNumber(value: string): boolean {
+    const regex = /^(?:0|[1-9]\d*)?(?:\.\d*)?$/;
+    return regex.test(value);
+  }
 }
